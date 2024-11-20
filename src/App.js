01282e3c74fcf82ca59c24ca361b5788108d327f
thelisanjay2399 <134526859+thelisanjay@users.noreply.github.com>
2024-11-20 11:43:19 +0530

    chnage

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import S3Uploader from './S3uploader';
+
+function App() {
+  return (
+    <div>
+    <div className="App">
+      <S3Uploader/>
+    </div>
+    </div>
+  );
+}
+
+export default App;
